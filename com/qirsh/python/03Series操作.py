c4254769ee8081f17f752492f34b037bf0fc3567
SQiRen <1434419753@qq.com>
2020-08-11 16:31:31 +0800

    04图形绘制

--- com/qirsh/python/03Series操作.py
+++ com/qirsh/python/03Series操作.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
-print(np.__version__)
-print(pd.__version__)
-a = pd.Series([2,0,-4,12])
-print(a)
+# print(np.__version__)
+# print(pd.__version__)
+# a = pd.Series([2,0,-4,12])
+# print(a)
+# a = np.arange(30).reshape(2,3,5)
+# print(a)
---
+++ com/qirsh/python/04图形绘制.py
@@ -0,0 +1,12 @@
+import math
+import matplotlib.pyplot as plt
+nbSamples = 256
+xRange = (-math.pi, math.pi)
+x, y = [], []
+for n in range(nbSamples):
+    ratio = (n + 0.5)/nbSamples
+    x.append(xRange[0] + (xRange[1] - xRange[0]) * ratio)
+    y.append(math.sin(x[-1]))
+
+plt.plot(x,y)
+plt.show()
